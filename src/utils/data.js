dec0a1ee560746cd0816d63db4f3931a922cda60
gremwiz1 <gremwiz@yandex.ru>
2021-10-27 23:38:53 +0300

    fix: delete file with data

--- src/utils/data.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-expressions */
-const dataOrder = {
-    number: "034536",
-    description: "идентификатор заказа",
-    status: "Ваш заказ начали готовить",
-    wait: "Дождитесь готовности на орбитальной станции",
-};
-export default dataOrder;
